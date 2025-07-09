f8d282785b7c1d73181be04ee03d721504bfcfcf
Henrique Sarhan <sarhansalomao@gmail.com>
2025-07-09 13:07:56 -0300

    WIP - First commit netlify adapter

--- veshin/svelte.config.js
+++ veshin/svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			edge: false,
+			split: true
+		})
+	},
 	prerender: {
 		handleHttpError: ({ path, referrer, message }) => {
 			// ignore deliberate link to shiny 404 page
