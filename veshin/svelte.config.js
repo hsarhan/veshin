b3ad8a57090cac9d3fdeb49540f613fe66502eec
Henrique Sarhan <sarhansalomao@gmail.com>
2025-07-09 15:27:47 -0300

    WIP - First commit netlify adapter

--- veshin/svelte.config.js
+++ veshin/svelte.config.js
@@ -2,19 +2,7 @@ import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: true
-		})
-	},
-	prerender: {
-		handleHttpError: ({ path, referrer, message }) => {
-			// ignore deliberate link to shiny 404 page
-			if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-				return;
-			}
-			throw new Error(message);
-		}
+		adapter: adapter({})
 	}
 };
 
