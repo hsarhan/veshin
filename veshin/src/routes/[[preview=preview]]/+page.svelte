81ad8106682a5a5c5fa2c2a9bc8ee9c34f452747
Henrique Sarhan <sarhansalomao@gmail.com>
2025-07-09 15:14:01 -0300

    WIP - First commit netlify adapter

--- veshin/src/routes/[[preview=preview]]/+page.svelte
+++ veshin/src/routes/[[preview=preview]]/+page.svelte
@@ -7,4 +7,8 @@
 	const { data }: PageProps = $props();
 </script>
 
+<svelte:head>
+	<title>{data.page.data.meta_title}</title>
+</svelte:head>
+
 <SliceZone slices={data.page.data.slices} {components} />
